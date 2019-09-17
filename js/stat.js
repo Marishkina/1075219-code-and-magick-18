bc4672bcaddd3413a953597b377c9f446998f61a
Marina <marishkina@gmail.com>
2019-09-17 22:14:51 +0800

    Задание 2

---
+++ js/stat.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var CLOUD_X = 155;
+var CLOUD_Y = 245;
+var GAP = 50;
+var BAR_WIDTH = 40;
+var TEXT_Y = 250;
+var BAR_HEIGHT = 150;
+var TEXT_GAP = 20;
+
+window.renderStatistics = function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.beginPath();
+  ctx.moveTo(110, 20);
+  ctx.lineTo(320, 30);
+  ctx.lineTo(530, 20);
+  ctx.lineTo(530, 290);
+  ctx.lineTo(320, 280);
+  ctx.lineTo(110, 290);
+  ctx.lineTo(110, 20);
+  ctx.closePath();
+  ctx.fill();
+
+  ctx.fillStyle = '#fff';
+  ctx.beginPath();
+  ctx.moveTo(100, 10);
+  ctx.lineTo(310, 20);
+  ctx.lineTo(520, 10);
+  ctx.lineTo(520, 280);
+  ctx.lineTo(310, 270);
+  ctx.lineTo(100, 280);
+  ctx.lineTo(100, 10);
+  ctx.closePath();
+  ctx.fill();
+
+
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillStyle = '#000';
+  ctx.fillText('Ура вы победили!', 130, 30);
+  ctx.fillText('Список результатов:', 130, 50);
+
+  var getMaxElement = function (arr) {
+    var maxElement = arr[0];
+
+    for (var i = 1; i < arr.length; i++) {
+      if (arr[i] > maxElement) {
+        maxElement = arr[i];
+      }
+    }
+    return maxElement;
+  };
+
+  ctx.fillStyle = '#000';
+
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < names.length; i++) {
+    if (i === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'hsl(240, ' + Math.floor(Math.random() * 100) + '%' + ', 50%)';
+    }
+    ctx.fillText(names[i], CLOUD_X + (BAR_WIDTH + GAP) * i, TEXT_Y);
+    ctx.fillText(Math.floor(times[i]), CLOUD_X + (BAR_WIDTH + GAP) * i, CLOUD_Y - ((BAR_HEIGHT * times[i]) / maxTime) - TEXT_GAP);
+    ctx.fillRect(CLOUD_X + (BAR_WIDTH + GAP) * i, CLOUD_Y - ((BAR_HEIGHT * times[i]) / maxTime), BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
+  }
+
+  // ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+
+  // ctx.fillStyle = 'hsl(240, ' + Math.floor(Math.random() * 100) + '%' + ', 50%)';
+};
+
