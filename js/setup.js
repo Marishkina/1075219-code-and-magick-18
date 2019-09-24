5c663e3710136f221dc2dc27544ad27f4523ad4a
Marina <marishkina@gmail.com>
2019-09-24 14:24:17 +0800

    Добавляет выбор персонажа

---
+++ js/setup.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var wizardFirstName = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var wizardSecondName = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var coatColors = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var userDialog = document.querySelector('.setup');
+
+var WIZARDS_COUNT = 4;
+
+
+userDialog.classList.remove('hidden');
+
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+.content
+.querySelector('.setup-similar-item');
+
+var getRandomItem = function (array) {
+  var randomItem = [Math.floor(Math.random() * array.length)];
+  return randomItem;
+};
+
+var renderRandomArray = function (wizardsCount) {
+
+  for (var i = 0; i < wizardsCount; i++) {
+    wizards [i] = {
+      name: getRandomItem(wizardFirstName) + ' ' + getRandomItem(wizardSecondName),
+      coatColor: getRandomItem(coatColors),
+      eyesColor: getRandomItem(eyesColors)
+    };
+  }
+};
+
+var wizards = renderRandomArray(WIZARDS_COUNT);
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+
+  return wizardElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
+
