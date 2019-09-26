effafc2f866a3a1c5bac009fd9daa0bbd2c8dc17
Marina <marishkina@gmail.com>
2019-09-26 17:20:00 +0800

    вносит правки в функцию getWizardsArray, init

--- js/setup.js
+++ js/setup.js
@@ -2,11 +2,9 @@
 
 var WIZARD_FIRST_NAME = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SECOND_NAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COUNT = 4;
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb 56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYE_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-var WIZARD_COUNT = 4;
-var wizards = [];
-
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
@@ -16,6 +14,7 @@ var getRandomItem = function (array) {
 };
 
 var getWizardsArray = function (wizardCount) {
+  var wizards = [];
   for (var i = 0; i < wizardCount; i++) {
     wizards[i] = {
       name: getRandomItem(WIZARD_FIRST_NAME) + ' ' + getRandomItem(WIZARD_SECOND_NAME),
@@ -23,7 +22,7 @@ var getWizardsArray = function (wizardCount) {
       eyesColor: getRandomItem(EYE_COLORS)
     };
   }
-  return getWizardsArray;
+  return wizards;
 };
 
 var renderWizard = function (wizard) {
@@ -37,7 +36,7 @@ var renderWizard = function (wizard) {
 };
 
 function init() {
-  getWizardsArray(WIZARD_COUNT);
+  var wizards = getWizardsArray(WIZARD_COUNT);
   var userDialog = document.querySelector('.setup');
   var fragment = document.createDocumentFragment();
 
